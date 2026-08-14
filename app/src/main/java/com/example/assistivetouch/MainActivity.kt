28311bb21efbf40b77a51e6fb172de623636e494
winnerfordinner5555 <102814051+winnerfordinner5555@users.noreply.github.com>
2026-08-14 23:18:42 +1000

    Update MainActivity.kt

--- app/src/main/java/com/example/assistivetouch/MainActivity.kt
+++ app/src/main/java/com/example/assistivetouch/MainActivity.kt
@@ -42,4 +42,4 @@ class MainActivity : AppCompatActivity() {
 
         status = TextView(this).apply {
             textSize = 15f
-            setPadding(0, 32, 0, 32
+            setPadding(0, 32, 0, 32)
